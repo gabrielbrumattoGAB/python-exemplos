4394595e22035f20467587cd78b334201f17c770
gabrielbrumattoGAB <gabriel.absenai@gmail.com>
2024-04-23 16:43:07 -0300

    Criação Variáveis.py

---
+++ Variaveis.py
@@ -0,0 +1,14 @@
+nome="Gabriel Alves Brumatto"
+idade=16
+email="gabrielbrumatto2007@gmail.com"
+telefone="17 991504046"
+nota=8,99
+aprovado=True
+print(nome)
+print(idade)
+print(email)
+print(telefone)
+print(nota)
+print(aprovado)
+
+
