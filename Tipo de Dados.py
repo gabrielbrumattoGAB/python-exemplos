0e32999ed7fa6e6d1ddeb65c0bbe3dce49fe0071
gabrielbrumattoGAB <gabriel.absenai@gmail.com>
2024-04-30 15:09:19 -0300

    Criação Tipo de Dados.py

---
+++ Tipo de Dados.py
@@ -0,0 +1,12 @@
+exemplo = 'Gabriel Brumatto'
+print(type(exemplo))
+
+exemplo = 23
+print(type(exemplo))
+
+exemplo = 9.99
+print(type(exemplo))
+      
+exemplo = True
+print(type(exemplo))
+      
