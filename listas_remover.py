d8fa30bb2f79439b7f03435b5ab4967045ce147b
gabrielbrumattoGAB <gabriel.absenai@gmail.com>
2024-05-23 09:35:52 -0300

    Criação listas_remover.py

---
+++ listas_remover.py
@@ -0,0 +1,14 @@
+alunos = ['Adriana', 'João', 'Maria', 'Pedro', 'Ana', 'Lucas', 'Mariana', 'Adriana']
+print(f"Lista de alunos: {alunos}")
+# Método Remove
+alunos.remove('Adriana')
+print(f"Lista de alunos: {alunos}")
+# Método Pop
+alunos.pop(2)
+print(f"Lista de alunos: {alunos}")
+# Função del
+del alunos [3]
+print(f"Lista de alunos: {alunos}")
+# Método clear
+alunos.clear()
+print(f"Lista de alunos: {alunos}")
