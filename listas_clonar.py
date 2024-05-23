4cdcb447a9682e5fffb0dba0d93106c26ddfc127
gabrielbrumattoGAB <gabriel.absenai@gmail.com>
2024-05-23 10:19:17 -0300

    Criação listas_clonar.py

---
+++ listas_clonar.py
@@ -0,0 +1,6 @@
+alunos = ['João', 'Maria', 'Pedro', 'Ana', 'Lucas', 'Mariana']
+copia = list(alunos)
+print(f"Lista original: {alunos}")
+print(f"Lista cópia: {copia}")
+alunos.remove('Maria')
+print(f"Lista alterada: {alunos}")
